e2a47d7ffb70601f00e2bb7db23a90c01f2be006
stevemkroll <steve.m.kroll@gmail.com>
2017-11-16 12:04:19 -0600

    functionality should work as intended

--- prevent_mobile_landscape.js
+++ prevent_mobile_landscape.js
@@ -3,18 +3,19 @@ exports.prevent_mobile_landscape = function() {
   var isMobile = false;
   var isLandscape = false;
 
-(function() {
-      let prevent_mobile_landscape = document.createElement("div");
-      prevent_mobile_landscape.setAttribute("id", "prevent_mobile_landscape");
-      prevent_mobile_landscape.style.height = '100vh';
-      prevent_mobile_landscape.style.width = '100vw';
-      prevent_mobile_landscape.style.background = '#666';
-      prevent_mobile_landscape.style.position = 'fixed';
-      prevent_mobile_landscape.style.top = '0px';
-      prevent_mobile_landscape.style.left = '0px';
-      prevent_mobile_landscape.style.zIndex = '999999999999';
-      document.body.appendChild(prevent_mobile_landscape);
-    })();
+  (function() {
+    let prevent_mobile_landscape = document.createElement("div");
+    prevent_mobile_landscape.setAttribute("id", "prevent_mobile_landscape");
+    prevent_mobile_landscape.style.height = '100vh';
+    prevent_mobile_landscape.style.width = '100vw';
+    prevent_mobile_landscape.style.background = '#666';
+    prevent_mobile_landscape.style.position = 'fixed';
+    prevent_mobile_landscape.style.top = '0px';
+    prevent_mobile_landscape.style.left = '0px';
+    prevent_mobile_landscape.style.zIndex = '999999999999';
+    prevent_mobile_landscape.style.display = 'none';
+    document.body.appendChild(prevent_mobile_landscape);
+  })();
 
   function preventMobileLandscape() {
     function detectMobile() {
@@ -26,12 +27,16 @@ exports.prevent_mobile_landscape = function() {
         navigator.userAgent.match(/IEMobile/)
       ) {
         isMobile = true;
+      } else {
+        isMobile = false;
       }
     }
 
     function detectLandscape() {
       if (window.innerWidth > window.innerHeight) {
         isLandscape = true;
+      } else {
+        isLandscape = false;
       }
     }
     detectMobile();
@@ -42,9 +47,11 @@ exports.prevent_mobile_landscape = function() {
       document.getElementById('prevent_mobile_landscape').style.display = "none";
     }
   }
+
   var supportsOrientationChange = "onorientationchange" in window,
     orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
   window.addEventListener(orientationEvent, function() {
     preventMobileLandscape();
   }, false);
+
 };
