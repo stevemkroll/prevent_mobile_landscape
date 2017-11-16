686f407436127e23e25bf68b2a639aa17373eebf
stevemkroll <steve.m.kroll@gmail.com>
2017-11-16 14:43:04 -0600

    added text

--- prevent_mobile_landscape.js
+++ prevent_mobile_landscape.js
@@ -4,16 +4,33 @@ exports.prevent_mobile_landscape = function() {
   var isLandscape = false;
 
   (function() {
-    let prevent_mobile_landscape = document.createElement("div");
-    prevent_mobile_landscape.setAttribute("id", "prevent_mobile_landscape");
+    const prevent_mobile_landscape = document.createElement('div');
+    prevent_mobile_landscape.setAttribute('id', 'prevent_mobile_landscape');
     prevent_mobile_landscape.style.height = '100vh';
     prevent_mobile_landscape.style.width = '100vw';
     prevent_mobile_landscape.style.background = '#666';
     prevent_mobile_landscape.style.position = 'fixed';
     prevent_mobile_landscape.style.top = '0px';
     prevent_mobile_landscape.style.left = '0px';
-    prevent_mobile_landscape.style.zIndex = '999999999999';
+    prevent_mobile_landscape.style.zIndex = '999999998';
+    prevent_mobile_landscape.style.overflow = 'hidden';
+    prevent_mobile_landscape.style.userSelect = 'none';
     prevent_mobile_landscape.style.display = 'none';
+  
+    const prevent_mobile_landscape_text = document.createElement('p');
+    prevent_mobile_landscape_text.setAttribute('id', 'prevent_mobile_landscape_text');
+    prevent_mobile_landscape_text.style.position = 'fixed';
+    prevent_mobile_landscape_text.style.top = '0px';
+    prevent_mobile_landscape_text.style.left = '0px';
+    prevent_mobile_landscape_text.style.lineHeight = '100vh';
+    prevent_mobile_landscape_text.style.height = '100vh';
+    prevent_mobile_landscape_text.style.width = '100vw';
+    prevent_mobile_landscape_text.style.textAlign = 'center';
+    prevent_mobile_landscape_text.style.color = '#FFF';
+    prevent_mobile_landscape_text.style.zIndex = '999999999';
+    prevent_mobile_landscape_text.innerHTML = 'Sorry, this device orientation is not supported';
+    
+    prevent_mobile_landscape.appendChild(prevent_mobile_landscape_text);
     document.body.appendChild(prevent_mobile_landscape);
   })();
 
