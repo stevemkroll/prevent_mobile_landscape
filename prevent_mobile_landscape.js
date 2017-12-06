da4e31702c05225b89607659091a7a2671de6a43
steve kroll <steve.m.kroll@gmail.com>
2017-12-06 16:15:28 -0600

    updated functionality to support user customization

--- prevent_mobile_landscape.js
+++ prevent_mobile_landscape.js
@@ -1,15 +1,38 @@
-exports.prevent_mobile_landscape = function(object) {
+exports.prevent_mobile_landscape = function(properties) {
 
   var isMobile = false;
   var isLandscape = false;
+
+  let defaults = {
+    background_color: 'linear-gradient(-90deg, #D77C7C, #AD71BC)',
+    text_color: '#eee',
+    font: 'Montserrat-Thin',
+    font_size: '20px',
+    text: 'Sorry, this device orientation is not supported'
+  }
+
+  let list = [
+      'background_color',
+      'text_color',
+      'font',
+      'font_size',
+      'text'
+    ];
+
+    (function checkProps(){
+      list.forEach(function(i){
+        switch(properties[i]){
+          case undefined:
+            properties[i] = defaults[i];
+        };
+      });
+    })();
 
   function createDivs() {
     var prevent_mobile_landscape = document.createElement('div');
     prevent_mobile_landscape.setAttribute('id', 'prevent_mobile_landscape');
     prevent_mobile_landscape.style.height = '100vh';
     prevent_mobile_landscape.style.width = '100vw';
-    // prevent_mobile_landscape.style.background = '#666';
-    prevent_mobile_landscape.style.background = object.background_color;
     prevent_mobile_landscape.style.position = 'fixed';
     prevent_mobile_landscape.style.top = '0px';
     prevent_mobile_landscape.style.left = '0px';
@@ -17,6 +40,7 @@ exports.prevent_mobile_landscape = function(object) {
     prevent_mobile_landscape.style.overflow = 'hidden';
     prevent_mobile_landscape.style.userSelect = 'none';
     prevent_mobile_landscape.style.display = 'none';
+    prevent_mobile_landscape.style.background = properties.background_color;
 
     var prevent_mobile_landscape_text = document.createElement('p');
     prevent_mobile_landscape_text.setAttribute('id', 'prevent_mobile_landscape_text');
@@ -27,11 +51,12 @@ exports.prevent_mobile_landscape = function(object) {
     prevent_mobile_landscape_text.style.height = '100vh';
     prevent_mobile_landscape_text.style.width = '100vw';
     prevent_mobile_landscape_text.style.textAlign = 'center';
-    prevent_mobile_landscape_text.style.color = '#FFF';
+    prevent_mobile_landscape_text.style.color = properties.text_color;
+    prevent_mobile_landscape_text.style.fontFamily = properties.font;
+    prevent_mobile_landscape_text.style.fontSize = properties.font_size;
     prevent_mobile_landscape_text.style.zIndex = '999999999';
-    prevent_mobile_landscape_text.style.fontFamily = object.font;
-    // prevent_mobile_landscape_text.innerHTML = 'Sorry, this device orientation is not supported';
-    prevent_mobile_landscape_text.innerHTML = object.custom_text;
+    prevent_mobile_landscape_text.style.margin = '0px';
+    prevent_mobile_landscape_text.innerHTML = properties.text;
 
     prevent_mobile_landscape.appendChild(prevent_mobile_landscape_text);
     document.body.appendChild(prevent_mobile_landscape);
@@ -69,7 +94,6 @@ exports.prevent_mobile_landscape = function(object) {
     } else {
       document.getElementById('prevent_mobile_landscape').style.display = "none";
       document.getElementsByTagName('body')[0].style.overflow = 'auto';
-
     }
   }
 
